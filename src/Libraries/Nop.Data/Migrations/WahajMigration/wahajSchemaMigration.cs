d6257f4624cf9b94ce0513f166ceed36a2de87f1
Rana Wahaj Ahmed <122843122+Rana-Wahaj@users.noreply.github.com>
2024-10-23 00:03:44 +0500

    init project

---
+++ src/Libraries/Nop.Data/Migrations/WahajMigration/wahajSchemaMigration.cs
@@ -0,0 +1,13 @@
+﻿using FluentMigrator;
+using Nop.Core.Domain.Catalog;
+using Nop.Data.Extensions;
+
+namespace Nop.Data.Migrations.WahajMigration;
+[NopSchemaMigration("October 18,2024, 11:35 AM", "Nop.Data wahaj base schema", MigrationProcessType.NoMatter)]
+internal class wahajSchemaMigration : ForwardOnlyMigration
+{
+    public override void Up()
+    {
+        Create.TableFor<CategoryWahaj>();
+    }
+}
