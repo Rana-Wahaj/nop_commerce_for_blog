66023cfaa84be1eaee367cacd8f69561e6ae62fc
Rana Wahaj Ahmed <122843122+Rana-Wahaj@users.noreply.github.com>
2024-10-27 01:51:45 +0500

    added AddProductToWahajCategoryModels

---
+++ src/Presentation/Nop.Web/Areas/Admin/Models/Catalog/AddProductToWahajCategoryModel.cs
@@ -0,0 +1,25 @@
+﻿using Nop.Web.Framework.Models;
+
+namespace Nop.Web.Areas.Admin.Models.Catalog;
+
+/// <summary>
+/// Represents a product model to add to the category
+/// </summary>
+public partial record AddProductToWahajCategoryModel : BaseNopModel
+{
+    #region Ctor
+
+    public AddProductToWahajCategoryModel()
+    {
+        SelectedProductIds = new List<int>();
+    }
+    #endregion
+
+    #region Properties
+
+    public int CategoryId { get; set; }
+
+    public IList<int> SelectedProductIds { get; set; }
+
+    #endregion
+}
