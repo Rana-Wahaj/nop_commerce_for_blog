2644c954f8be5bff40cdc58199a13bf18a31fe29
Rana Wahaj Ahmed <122843122+Rana-Wahaj@users.noreply.github.com>
2024-10-27 01:50:00 +0500

    added WahajCategoryModels.

---
+++ src/Presentation/Nop.Web/Areas/Admin/Models/Catalog/WahajCategoryProductSearchModel.cs
@@ -0,0 +1,12 @@
+﻿using Nop.Web.Framework.Models;
+
+namespace Nop.Web.Areas.Admin.Models.Catalog;
+
+public partial record WahajCategoryProductSearchModel : BaseSearchModel
+{
+    #region Properties
+
+    public int CategoryId { get; set; }
+
+    #endregion
+}
